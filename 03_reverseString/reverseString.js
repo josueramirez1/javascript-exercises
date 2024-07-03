3582a2f97f94bc492b52b354c92be26d43a9330f
josueramirez1 <josue67ramirez@gmail.com>
2024-07-02 20:30:24 -0700

    Adding solution two and three
    completing assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = [];
+  for (let i = 0; i < string.length; i++) {
+    let slicedLetter = string.slice(i, i + 1);
+    reversedString.unshift(slicedLetter);
+  }
+  return reversedString.join("");
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
