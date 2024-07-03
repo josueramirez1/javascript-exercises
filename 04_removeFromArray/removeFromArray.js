58f136b5556364d5b8f4e610db915c825e8705bb
josueramirez1 <josue67ramirez@gmail.com>
2024-07-03 14:33:57 -0700

    adding solution to number 4
    used filter and includes method to find values to filter through

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...others) {
+  //   let newArr = [...array, ...others];
+  let newArr = array.filter((item) => {
+    if (!others.includes(item)) {
+      return item;
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
