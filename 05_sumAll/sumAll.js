b15554dcafada02fcd8ca43d5e932911384629e2
josueramirez1 <josue67ramirez@gmail.com>
2024-07-03 17:00:21 -0700

    solution to problem five
    completing sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  //conditions for running properly
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
 
+  // creating array for later reducing
+  let arr = [];
+  //if larger number is passed through first...
+  if (num1 > num2) {
+    for (let i = num2; i <= num1; i++) {
+      arr.push(i);
+    }
+  } else
+    for (let i = num1; i <= num2; i++) {
+      arr.push(i);
+    }
+
+  //using reduce method
+  let initialValue = 0;
+
+  let sumOfAllNum = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, initialValue);
+  return sumOfAllNum;
 };
+
+sumAll(5, 7);
 
 // Do not edit below this line
 module.exports = sumAll;
