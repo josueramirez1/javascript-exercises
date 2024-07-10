228df9e948c54fcded77fe23eddf7ed858a10eb6
josueramirez1 <josue67ramirez@gmail.com>
2024-07-10 10:55:33 -0700

    adding solution to palindrome
    used regex and lowerCase to remove unecessary parameters and find if reversed string is true

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+  let punctuationless = string.replace(regex, "");
+  let noWhiteSpace = punctuationless.replace(/\s+/g, "");
+  let finalString = noWhiteSpace.toLowerCase();
 
+  let reversed = finalString.split("").reverse().join("");
+
+  if (finalString === reversed) return true;
+  return false;
 };
 
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
