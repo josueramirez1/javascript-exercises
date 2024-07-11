110ce26bab2cbba78f73be1c3fc03f2f366fd554
josueramirez1 <josue67ramirez@gmail.com>
2024-07-11 14:50:25 -0700

    correctin and adding comments to remind myself and understand code btter

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,18 +3,21 @@ const findTheOldest = function (arr) {
   let death = "yearOfDeath";
   let oldest;
 
-  // Finds the oldest person if yearOfDeath is undefined on non-oldest or oldest
+  // Finds the oldest person if yearOfDeath is undefined on non-oldest or oldest and places it first on the array of objects
 
   for (let obj of arr) {
     if (death in obj === false) {
-      oldest = arr.sort((first, next) => {
+      arr.sort((first, next) => {
         return next.yearOfBirth > first.yearOfBirth ? -1 : 1;
       });
     }
   }
+
   oldest = arr.sort((first, next) => {
     let firstPersonAlive = first.yearOfDeath - first.yearOfBirth;
+    console.log(firstPersonAlive);
     let nextPersonAlive = next.yearOfDeath - next.yearOfBirth;
+    console.log(nextPersonAlive);
     return firstPersonAlive > nextPersonAlive ? -1 : 1;
   });
 
@@ -22,6 +25,25 @@ const findTheOldest = function (arr) {
 
   return oldest[0];
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
