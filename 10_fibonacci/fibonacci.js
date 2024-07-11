84bd40ab5d51d011631c3761e78362335d04567b
josueramirez1 <josue67ramirez@gmail.com>
2024-07-10 20:48:56 -0700

    adding solution to assignment 10
    fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  // Change string to number
+  let num = parseInt(n);
 
+  // restrictions
+  if (num === 0) return 0;
+  if (num < 0) return "OOPS";
+
+  // variables for fibonacci
+  let arr = [];
+  let sum = 0;
+  // start the loop
+  for (let i = 0; i < num; i++) {
+    // if array is empty...
+    if (!arr[0]) {
+      arr.push(i + 1);
+      arr.push(i + 1);
+    }
+    // if array has two values...
+    sum = arr[i] + arr[i + 1];
+    arr.push(sum);
+  }
+  return arr[num - 1];
 };
 
 // Do not edit below this line
