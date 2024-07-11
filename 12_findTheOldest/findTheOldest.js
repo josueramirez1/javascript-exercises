4d15d9dbafec5abe8ef251551ff70aa34da06311
josueramirez1 <josue67ramirez@gmail.com>
2024-07-11 12:57:19 -0700

    adding solution to assignment 12
    finding the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  // variables
+  let death = "yearOfDeath";
+  let oldest;
 
+  // Finds the oldest person if yearOfDeath is undefined on non-oldest or oldest
+
+  for (let obj of arr) {
+    if (death in obj === false) {
+      oldest = arr.sort((first, next) => {
+        return next.yearOfBirth > first.yearOfBirth ? -1 : 1;
+      });
+    }
+  }
+  oldest = arr.sort((first, next) => {
+    let firstPersonAlive = first.yearOfDeath - first.yearOfBirth;
+    let nextPersonAlive = next.yearOfDeath - next.yearOfBirth;
+    return firstPersonAlive > nextPersonAlive ? -1 : 1;
+  });
+
+  // return oldest person
+
+  return oldest[0];
 };
 
 // Do not edit below this line
