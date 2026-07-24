759e7206100d85ed12c35d3d44ae4e521a47f948
josueramirez1 <josue67ramirez@gmail.com>
2026-07-24 14:57:12 -0700

    submitting 1-11 solutions

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const arr = string.split("");
+  let reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+  return reversed.join("");
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let result = 0;
+  let first = num1;
+  let second = num2;
 
+  if (num1 > num2) {
+    first = num2;
+    second = num1;
+  }
+
+  if (first < 0 || second < 0) {
+    return "ERROR";
+  }
+
+  if (typeof first !== "number" || typeof second !== "number") {
+    return "ERROR";
+  }
+
+  if (!Number.isInteger(first) || !Number.isInteger(second)) {
+    return "ERROR";
+  }
+
+  for (let i = first; i <= second; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
