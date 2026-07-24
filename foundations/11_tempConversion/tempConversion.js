36799e7f655c8d3d15b70171ddb3030ae5f7e0eb
josueramirez1 <josue67ramirez@gmail.com>
2026-07-24 15:27:10 -0700

    adding solution 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  //(0°C × 9/5) + 32
+  let celsiusNum = Number((num - 32) / (9 / 5));
+  if (!Number.isInteger(celsiusNum)) {
+    return Number(celsiusNum.toFixed(1));
+  }
+  return celsiusNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  //(32°F − 32) × 5/9
+  let fahrenheitNum = Number(num * 1.8 + 32);
+
+  if (!Number.isInteger(fahrenheitNum)) {
+    return Number(fahrenheitNum.toFixed(1));
+  }
+  return fahrenheitNum;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
